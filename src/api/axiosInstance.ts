3c80158e6ea0f2a7f586acd2d72d9e817cb039bf
akhan <akhan.dulatbay@gmail.com>
2025-10-30 09:11:32 +0500

    refactor: remove placeholder text from admin login fields and clean up routing

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getToken, getAdminAuth} from "../utils/tokenUtils.ts";
 
-const BASE_URL = "http://localhost:8080"; // http://localhost:8080
+const BASE_URL = "https://meet-akim.kz"; // http://localhost:8080
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
